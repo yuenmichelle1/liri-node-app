4bf65df0c778db21ff187f43a233a8426a2ac2ef
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-01-29 19:59:36 -0600

    initial files

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// commands
+// * `my-tweets`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
