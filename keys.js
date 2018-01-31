1fccb7cb16108decf6720e5788da20f807ed6cbe
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-01-31 00:08:51 -0600

    git ignore

--- keys.js
+++ keys.js
@@ -11,3 +11,8 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+module.exports{
+  
+}
--- liri.js
+++ liri.js
@@ -2,11 +2,29 @@ require("dotenv").config();
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+var grabKeys= require("./keys.js");
+
 // commands
 // * `my-tweets`
+
 
 // * `spotify-this-song`
 
 // * `movie-this`
 
 // * `do-what-it-says`
+var command = process.argv[2];
+switch (command){
+    case "my-tweets":
+        //show your last 20 tweets
+        console.log('tweet');
+        break;
+    case "spotify-this-song":
+        console.log("spotify this sonh");   
+        break;
+    case "movie-this":
+        console.log("movie-whatever");
+        break;
+    case "do-what-it-says":
+        console.log("do what i say");        
+}
