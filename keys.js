24dbcd1a92312880d6c8ccdce5e0d1205e0e6ea9
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-01-31 12:06:25 -0600

    got spotify to work finally

--- keys.js
+++ keys.js
@@ -11,4 +11,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
--- liri.js
+++ liri.js
@@ -7,9 +7,6 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 // commands
-// * `my-tweets`--show last 20 tweets
-
-
 // * `spotify-this-song` ---show info of the song in terminal (artist, song name, preview link of song in spotify, album the song is form)
 //-- if no song is available default is Ace of Base. The Sign
 
@@ -21,7 +18,6 @@ var command = process.argv[2];
 switch (command){
     case "my-tweets":
         //show your last 20 tweets
-        console.log('tweet');
         var params = {
             screen_name: 'thug_dev_life',
             count: 20
@@ -29,8 +25,10 @@ switch (command){
 
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
           if (!error) {
+            console.log('These were my last 20 tweets');  
             for (var i=0; i< tweets.length; i++){
-                console.log(tweets[i].text);
+                console.log(`_______________________________________________________` );
+                console.log(`${i+1}: ${tweets[i].text}`);
             }
           } else {
               console.log(error);
@@ -38,7 +36,33 @@ switch (command){
         });
         break;
     case "spotify-this-song":
-        console.log("spotify this sonh");   
+        var userSong = process.argv.slice(3).join(' ');
+        console.log(userSong); 
+        if (userSong == ""){
+            spotify
+            .search({ type: 'track', query: `The Sign Ace Of Base`, limit: 1})
+            .then(function(response) {
+                console.log(`Song not entered so I picked one for you!`);
+                showSongInfo(response);
+                })
+        }else{
+            spotify
+            .search({ type: 'track', query: userSong, limit: 1})
+            .then(function(response) {
+                console.log(`Here is the first song when searching for "${userSong}"`);
+                showSongInfo(response);
+            })
+            .catch(function(err) {
+                userSong = "The Sign Ace of Base";
+                console.log(userSong);
+                spotify
+                .search({ type: 'track', query: `The Sign Ace Of Base`, limit: 1})
+                .then(function(response) {
+                    console.log(`Song not found so I picked one for you!`);
+                    showSongInfo(response);
+                })
+            });
+        }    
         break;
     case "movie-this":
         console.log("movie-whatever");
@@ -46,4 +70,14 @@ switch (command){
     case "do-what-it-says":
         console.log("do what i say"); 
 
+}
+
+
+function showSongInfo(response){
+    console.log(`______________________________________________________`);
+    console.log(`Artist:  ${response.tracks.items[0].artists[0].name}`);
+    console.log(`Name of Song: ${response.tracks.items[0].name}`);
+    console.log(`Check out the song here : ${response.tracks.items[0].external_urls.spotify}`);
+    console.log(`Album: ${response.tracks.items[0].album.name}`)
+    console.log(`__________________________________________________________`);
 }
