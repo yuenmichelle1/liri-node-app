b7b3af91e682b710998a397cb3da72778148881d
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-01-31 10:09:41 -0600

    twitter works

--- liri.js
+++ liri.js
@@ -1,15 +1,10 @@
 require("dotenv").config();
 
 var keys= require("./keys.js");
-
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-
-
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
-
 
 // commands
 // * `my-tweets`--show last 20 tweets
@@ -22,19 +17,33 @@ var client = new Twitter(keys.twitter);
 
 // * `do-what-it-says`
 var command = process.argv[2];
-console.log(command);
-// switch (command){
-//     case "my-tweets":
-//         //show your last 20 tweets
-//         console.log('tweet');
-//         break;
-//     case "spotify-this-song":
-//         console.log("spotify this sonh");   
-//         break;
-//     case "movie-this":
-//         console.log("movie-whatever");
-//         break;
-//     case "do-what-it-says":
-//         console.log("do what i say"); 
 
-// }
+switch (command){
+    case "my-tweets":
+        //show your last 20 tweets
+        console.log('tweet');
+        var params = {
+            screen_name: 'thug_dev_life',
+            count: 20
+        };
+
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+          if (!error) {
+            for (var i=0; i< tweets.length; i++){
+                console.log(tweets[i].text);
+            }
+          } else {
+              console.log(error);
+          }
+        });
+        break;
+    case "spotify-this-song":
+        console.log("spotify this sonh");   
+        break;
+    case "movie-this":
+        console.log("movie-whatever");
+        break;
+    case "do-what-it-says":
+        console.log("do what i say"); 
+
+}
